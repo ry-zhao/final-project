b56e2c045dccd5f94a0dbe78fab9730501d0fa98
ry-zhao <ryanzhao21@gmail.com>
2021-11-05 13:13:55 -0700

    Deleted commented-out code.

--- client/index.jsx
+++ client/index.jsx
@@ -2,27 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-// ReactDOM.render(
-//   <>
-//     <nav>
-//       <div className="nav-wrapper bg-columbia-blue">
-//         <a href="#" className="brand-logo center bright-gray">Rooms</a>
-//       </div>
-//     </nav>
-//   </>,
-//   document.querySelector('header')
-// );
-
-// ReactDOM.render(
-//   <>
-//     <div className="container">
-//       <div className="row">
-//       </div>
-//     </div>
-//   </>,
-//   document.querySelector('footer')
-// );
-
 ReactDOM.render(
   <>
     <App />
